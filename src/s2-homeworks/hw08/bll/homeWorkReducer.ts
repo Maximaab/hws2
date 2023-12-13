27d58c59c916b4a8cb4e732a44b2a835aef9fdbb
Maximaab <141754764+Maximaab@users.noreply.github.com>
2023-12-13 22:07:45 +0200

    Commit 13.12

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -5,16 +5,25 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': {
-           // by name
+            // by name
+            let copyState = [...state]
+            if (action.payload === "up") {
 
-            return state // need to fix
+                copyState.sort((a, b) => a.name.localeCompare(b.name))
+            } else if (action.payload === "down") {
+
+                copyState.sort((a, b) => b.name.localeCompare(a.name))
+            }
+            return copyState // need to fix
         }
         case 'check': {
-
-            return state // need to fix
+            let copyState = [...state]
+            if (action.payload >=18)
+            copyState = copyState.filter(el=>el.age > 18)
+            return copyState // need to fix
         }
         default:
             return state
