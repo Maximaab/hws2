f297af41fbbe18903451d4fbb767cbde858ecf17
Maximaab <141754764+Maximaab@users.noreply.github.com>
2024-01-13 18:39:14 +0200

    Commit 13.01

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -39,8 +39,8 @@ function Clock() {
     const stringDate = date.toLocaleDateString("ru-RU") || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
-    const stringDay = date?.toLocaleString('ru-RU', {weekday: 'long'}) || <br/> // пишут студенты
-    const stringMonth = date?.toLocaleString('ru-RU', {month: 'long'}) || <br/> // пишут студенты
+    const stringDay = date?.toLocaleString('en-US', {weekday: 'long'}) || <br/> // пишут студенты
+    const stringMonth = date?.toLocaleString('en-US', {month: 'long'}) || <br/> // пишут студенты
 
     return (
         <div className={s.clock}>
